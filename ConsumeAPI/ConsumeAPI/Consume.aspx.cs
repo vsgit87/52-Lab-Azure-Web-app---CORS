2859e8a0735da0cec755e75f73b1071361a63db8
vignesh <vigneshsmit@gmail.com>
2020-12-15 14:46:17 +1300

    twelvth

---
+++ ConsumeAPI/ConsumeAPI/Consume.aspx.cs
@@ -0,0 +1,52 @@
+﻿using mvcapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumeAPI
+{
+    public partial class Consume : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = "api/Course";
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://webapi9000.azurewebsites.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync(path);
+                response.Wait();
+
+                var result= response.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var CourseGet = result.Content.ReadAsAsync<Course[]>();
+                    CourseGet.Wait();
+
+                    var CourseList = CourseGet.Result;
+                    
+                    foreach(Course obj in CourseList)
+                    {
+                        Response.Write("Course Name : ");
+                        Response.Write(obj.Name);
+                        Response.Write("</br>");
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
---
+++ webapi/webapi/Controllers/CourseController.cs
@@ -0,0 +1,53 @@
+﻿using mvcapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapi.Controllers
+{
+    public class CourseController : ApiController
+    {
+        List<Course> dtset;
+
+        public CourseController()
+        {
+            dtset = new List<Course>()
+            {
+                new Course {Id=1,Name="AZ-104", Description="Microsoft Azure Administrator", Rating=4.5},
+                new Course {Id=2,Name="AZ-204", Description="Microsoft Azure Developer", Rating=4.6},
+                new Course {Id=3,Name="AZ-303", Description="Microsoft Azure Architect", Rating=4.7}
+            };
+
+        }
+        // GET: api/Course
+        public List<Course> Get()
+        {
+            return dtset;
+        }
+
+        // GET: api/Course/5
+        public Course Get(int id)
+        {
+            Course obj = dtset.FirstOrDefault(c => c.Id == id);
+            return obj;
+        }
+
+        // POST: api/Course
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Course/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Course/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
